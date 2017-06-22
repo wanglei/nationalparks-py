8d76c97dddb964eb497dee30482c1967df05bbbd
wanglei <wanglei321@gmail.com>
2017-06-22 03:22:25 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'WorldWide National Parks (PY+1)',
+        'displayName': 'WorldWide National Parks (PY+2)',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
